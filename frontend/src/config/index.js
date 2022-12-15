51fedd853b2fd489ad0c05642fe073d38fc2afe4
ArshlanSiddique1 <118898279+ArshlanSiddique1@users.noreply.github.com>
2022-12-15 10:22:01 +0530

    Add files via upload

---
+++ frontend/src/config/index.js
@@ -0,0 +1,17 @@
+// For server
+// export const API_URL = "http://107.191.58.235:3002/v1";
+// export const SERVER_URL = "http://107.191.58.235:3002/images/";
+// export const API_URL_SOCKET = "http://107.191.58.235:3002/";
+
+// For Local
+export const API_URL = "http://localhost:8000/api/v1";
+export const SERVER_URL = "http://localhost:8002/images/";
+export const API_URL_SOCKET = "http://localhost:8002/";
+
+export const API_KEY = "AIzaSyBMCqT0R-ZC_DMMRKnPWIDFWsluaeGfTHE";
+
+export const MAX_DEVICE = 10;
+
+export const COST_PER_DEVICE = 100;
+
+export const NO_OF_MONTHS = 12;
